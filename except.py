6b77503fc9db343a5cb394a6fa765529e6418920
theicecub <banan8936@gmail.com>
2024-12-07 21:22:31 +0500

    commiting hometask

--- except.py
+++ except.py
@@ -1,15 +1,24 @@
-try:
-    result = []
-    def divider(a, b):
-        if a < b:
-            raise ValueError
-        if b > 100:
-            raise IndexError
+def divider(a, b):
+    if a < b:
+        return ValueError("Second number can not be higher than first number")
+    elif b > 100:
+        return IndexError("Second number can not be more than 100")
+    else:
         return a/b
 
-data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
-for key in data:
-    res = divider(key, data[key])
-    result.append(res)
+print(divider(4, 2))
+print(divider(4, 8))
+print(divider(105, 104))
 
-print(result)
+
+try:
+    result = []
+    data = [10, 2, 2, 5, 4, 18, 0, 15, 8, 4]
+    for key in data:
+        res = divider(key, data[key])
+        result.append(res)
+    print(result)
+except (IndexError, ValueError):
+    print("There is an index or value error in your code")
+except (ZeroDivisionError):
+    print("You can't divide to zero")
